53a73bc0eb52df986ee8fac4bcc3cee95f8600c7
NIVASINI <156501457+nivasini03@users.noreply.github.com>
2025-06-26 12:36:24 +0530

    Update 06 Priority encoder .v

--- 1. Verilog language /4. Procedures /06 Priority encoder .v
+++ 1. Verilog language /4. Procedures /06 Priority encoder .v
@@ -1,1 +1,14 @@
-
+module top_module (
+    input [3:0] in,
+    output reg [1:0] pos
+);
+always @(*) begin			
+		case (in)
+			4'b1000:pos = 2'd3;
+            4'b1100,4'b0100 : pos = 2'd2;
+            4'b1110,4'b0010,4'b0110,4'b1010 : pos = 2'd1;
+			default: pos = 2'b0;
+		endcase
+	end
+	
+endmodule
