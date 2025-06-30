bd8e4799e945b29319a7d668b52df21c02c3fa51
NIVASINI <156501457+nivasini03@users.noreply.github.com>
2025-06-30 10:15:35 +0530

    Update 17 Even longer vectors .v

--- 2. Circuits/1. Combinational Logic /1. Basic Gates/17 Even longer vectors .v
+++ 2. Circuits/1. Combinational Logic /1. Basic Gates/17 Even longer vectors .v
@@ -1,1 +1,27 @@
+module top_module( 
+    input [99:0] in,
+    output reg [98:0] out_both,
+    output reg [99:1] out_any,
+    output reg [99:0] out_different );
 
+    integer i;
+    always @(*) begin
+        
+        for(i = 0; i <= 98; i = i + 1) begin
+            out_both[i] = in[i] & in[i+1];
+        end
+
+        for(i = 1; i <= 99; i = i + 1) begin
+            out_any[i] = in[i] | in[i-1];
+        end
+
+        
+        for(i = 0; i <= 98; i = i + 1) begin
+            out_different[i] = in[i] ^ in[i+1];
+        end
+
+       
+        out_different[99] = in[99] ^ in[0];
+    end
+
+endmodule
